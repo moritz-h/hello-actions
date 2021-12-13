5e28fec52afb492c748241b607b7c47126b127c0
Moritz Heinemann <moritz.heinemann@visus.uni-stuttgart.de>
2021-12-13 19:48:25 +0100

    color

--- .ci/run_clang_format.sh
+++ .ci/run_clang_format.sh
@@ -2,5 +2,5 @@
 
 echo "Hello"
 clang-format-12 --version
-clang-format-12 main.cpp | diff --color -u main.cpp -
+clang-format-12 main.cpp | diff --color=always -u main.cpp -
 #clang-format-12 -i main.cpp
