4cee65c92d948f1d9bcec362a968ceb4e6252829
Moritz Heinemann <moritz.heinemann@visus.uni-stuttgart.de>
2021-12-13 20:18:12 +0100

    output

--- .ci/run_clang_format.sh
+++ .ci/run_clang_format.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "::error file=main.cpp::Hello"
+echo "::error file=main.cpp,line=5,col=1,endColumn=19::Hello"
 clang-format-12 --version
 clang-format-12 main.cpp | diff --color=always -u main.cpp -
 #clang-format-12 -i main.cpp
